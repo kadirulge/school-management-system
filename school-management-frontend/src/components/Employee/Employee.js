9e7316b97bf36678b1c820b68dc686ab88f4b3ce
kadirulge <kadirulge7@gmail.com>
2023-07-17 13:47:00 +0300

    update Employee.js and Student.js

--- school-management-frontend/src/components/Employee/Employee.js
+++ school-management-frontend/src/components/Employee/Employee.js
@@ -2,16 +2,123 @@ import React, { useState, useEffect } from "react";
 
 function Employee() {
   const [employees, setEmployees] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editedEmployee, setEditedEmployee] = useState({});
+  const [newEmployee, setNewEmployee] = useState({
+    firstName: "",
+    lastName: "",
+    address: "",
+    phoneNumber: "",
+    employeeNumber: "",
+    manager: false,
+    jobTitle: "",
+    schoolId: "",
+  });
+
+  const jobTitles = [
+    "TEACHER",
+    "PRINCIPAL",
+    "VICE_PRINCIPAL",
+    "JANITOR",
+    "COOK",
+    "SECRETARY",
+    "NURSE",
+    "LIBRARIAN",
+    "COUNSELOR",
+    "ADMINISTRATOR",
+    "BUS_DRIVER",
+  ];
 
   useEffect(() => {
     fetchEmployees();
-  }, []);
+  }, [editMode]);
 
   const fetchEmployees = async () => {
     try {
       const response = await fetch("/api/employees");
       const data = await response.json();
       setEmployees(data);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleEdit = (employee) => {
+    setEditMode(true);
+    setEditedEmployee(employee);
+  };
+
+  const handleCancel = () => {
+    setEditMode(false);
+    setEditedEmployee({});
+  };
+
+  const handleSave = async () => {
+    try {
+      const response = await fetch(`/api/employees/${editedEmployee.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(editedEmployee),
+      });
+
+      if (response.ok) {
+        console.log("Updated:", editedEmployee);
+        setEditMode(false);
+        setEditedEmployee({});
+      } else {
+        console.error("Update failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch(`/api/employees/${id}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        console.log("Deleted:", id);
+        fetchEmployees();
+      } else {
+        console.error("Delete failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleCreate = async () => {
+    try {
+      const response = await fetch("/api/employees", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newEmployee),
+      });
+
+      if (response.ok) {
+        const createdEmployee = await response.json();
+        setEmployees([...employees, createdEmployee]);
+        console.log("Created:", createdEmployee);
+        setNewEmployee({
+          firstName: "",
+          lastName: "",
+          address: "",
+          phoneNumber: "",
+          employeeNumber: "",
+          manager: false,
+          jobTitle: "",
+          schoolId: "",
+        });
+      } else {
+        console.error("Create failed");
+      }
     } catch (error) {
       console.error("Error:", error);
     }
@@ -52,6 +159,9 @@ function Employee() {
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     School Id
                   </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Actions
+                  </th>
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
@@ -61,34 +171,262 @@ function Employee() {
                       {employee.id}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {employee.firstName}
+                      {editMode && editedEmployee.id === employee.id ? (
+                        <input
+                          type="text"
+                          className="px-2 py-1 border rounded-lg"
+                          value={editedEmployee.firstName || ""}
+                          onChange={(e) =>
+                            setEditedEmployee({
+                              ...editedEmployee,
+                              firstName: e.target.value,
+                            })
+                          }
+                        />
+                      ) : (
+                        employee.firstName
+                      )}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {employee.lastName}
+                      {editMode && editedEmployee.id === employee.id ? (
+                        <input
+                          type="text"
+                          className="px-2 py-1 border rounded-lg"
+                          value={editedEmployee.lastName || ""}
+                          onChange={(e) =>
+                            setEditedEmployee({
+                              ...editedEmployee,
+                              lastName: e.target.value,
+                            })
+                          }
+                        />
+                      ) : (
+                        employee.lastName
+                      )}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {employee.address}
+                      {editMode && editedEmployee.id === employee.id ? (
+                        <input
+                          type="text"
+                          className="px-2 py-1 border rounded-lg"
+                          value={editedEmployee.address || ""}
+                          onChange={(e) =>
+                            setEditedEmployee({
+                              ...editedEmployee,
+                              address: e.target.value,
+                            })
+                          }
+                        />
+                      ) : (
+                        employee.address
+                      )}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {employee.phoneNumber}
+                      {editMode && editedEmployee.id === employee.id ? (
+                        <input
+                          type="text"
+                          className="px-2 py-1 border rounded-lg"
+                          value={editedEmployee.phoneNumber || ""}
+                          onChange={(e) =>
+                            setEditedEmployee({
+                              ...editedEmployee,
+                              phoneNumber: e.target.value,
+                            })
+                          }
+                        />
+                      ) : (
+                        employee.phoneNumber
+                      )}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {employee.employeeNumber}
+                      {editMode && editedEmployee.id === employee.id ? (
+                        <input
+                          type="text"
+                          className="px-2 py-1 border rounded-lg"
+                          value={editedEmployee.employeeNumber || ""}
+                          onChange={(e) =>
+                            setEditedEmployee({
+                              ...editedEmployee,
+                              employeeNumber: e.target.value,
+                            })
+                          }
+                        />
+                      ) : (
+                        employee.employeeNumber
+                      )}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {employee.manager ? "Yes" : "No"}
+                      {editMode && editedEmployee.id === employee.id ? (
+                        <input
+                          type="checkbox"
+                          checked={editedEmployee.manager}
+                          onChange={(e) =>
+                            setEditedEmployee({
+                              ...editedEmployee,
+                              manager: e.target.checked,
+                            })
+                          }
+                        />
+                      ) : employee.manager ? (
+                        "Yes"
+                      ) : (
+                        "No"
+                      )}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {employee.jobTitle}
+                      {editMode && editedEmployee.id === employee.id ? (
+                        <select
+                          className="px-2 py-1 border rounded-lg"
+                          value={editedEmployee.jobTitle || ""}
+                          onChange={(e) =>
+                            setEditedEmployee({
+                              ...editedEmployee,
+                              jobTitle: e.target.value,
+                            })
+                          }
+                        >
+                          <option value="">Select Job Title</option>
+                          {jobTitles.map((jobTitle) => (
+                            <option key={jobTitle} value={jobTitle}>
+                              {jobTitle}
+                            </option>
+                          ))}
+                        </select>
+                      ) : (
+                        employee.jobTitle
+                      )}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
                       {employee.schoolId}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {editMode && editedEmployee.id === employee.id ? (
+                        <>
+                          <button
+                            className="mr-2 text-blue-500"
+                            onClick={handleSave}
+                          >
+                            Save
+                          </button>
+                          <button
+                            className="text-red-500"
+                            onClick={handleCancel}
+                          >
+                            Cancel
+                          </button>
+                        </>
+                      ) : (
+                        <button
+                          className="text-blue-500"
+                          onClick={() => handleEdit(employee)}
+                        >
+                          Edit
+                        </button>
+                      )}
+                      {!editMode && (
+                        <button
+                          className="text-red-500 ms-3"
+                          onClick={() => handleDelete(employee.id)}
+                        >
+                          Delete
+                        </button>
+                      )}
                     </td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
+        </div>
+      </div>
+      <div className="my-4 ms-8">
+        <h2 className="text-gray-800 text-lg font-semibold text-gray mb-1">
+          Create New Employee
+        </h2>
+        <div className="flex">
+          <input
+            type="text"
+            className="px-2 py-1 border rounded-lg mr-2"
+            placeholder="First Name"
+            value={newEmployee.firstName}
+            onChange={(e) =>
+              setNewEmployee({ ...newEmployee, firstName: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            className="px-2 py-1 border rounded-lg mr-2"
+            placeholder="Last Name"
+            value={newEmployee.lastName}
+            onChange={(e) =>
+              setNewEmployee({ ...newEmployee, lastName: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            className="px-2 py-1 border rounded-lg mr-2"
+            placeholder="Address"
+            value={newEmployee.address}
+            onChange={(e) =>
+              setNewEmployee({ ...newEmployee, address: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            className="px-2 py-1 border rounded-lg mr-2"
+            placeholder="Phone Number"
+            value={newEmployee.phoneNumber}
+            onChange={(e) =>
+              setNewEmployee({ ...newEmployee, phoneNumber: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            className="px-2 py-1 border rounded-lg mr-2"
+            placeholder="Employee Number"
+            value={newEmployee.employeeNumber}
+            onChange={(e) =>
+              setNewEmployee({ ...newEmployee, employeeNumber: e.target.value })
+            }
+          />
+          <select
+            className="px-2 py-1 border rounded-lg mr-2"
+            value={newEmployee.jobTitle}
+            onChange={(e) =>
+              setNewEmployee({ ...newEmployee, jobTitle: e.target.value })
+            }
+          >
+            <option value="">Select Job Title</option>
+            {jobTitles.map((jobTitle) => (
+              <option key={jobTitle} value={jobTitle}>
+                {jobTitle}
+              </option>
+            ))}
+          </select>
+          <input
+            type="text"
+            className="px-2 py-1 border rounded-lg mr-2"
+            placeholder="School Id"
+            value={newEmployee.schoolId}
+            onChange={(e) =>
+              setNewEmployee({ ...newEmployee, schoolId: e.target.value })
+            }
+          />
+          <label className="mt-2 mr-2">Is Manager</label>
+          <input
+            type="checkbox"
+            className="mr-2"
+            checked={newEmployee.manager}
+            onChange={(e) =>
+              setNewEmployee({ ...newEmployee, manager: e.target.checked })
+            }
+          />
+          <button
+            className="px-4 py-2 bg-green-500 text-white rounded-lg"
+            onClick={handleCreate}
+          >
+            Create
+          </button>
         </div>
       </div>
     </div>
--- school-management-frontend/src/components/Student/Student.js
+++ school-management-frontend/src/components/Student/Student.js
@@ -2,10 +2,21 @@ import React, { useState, useEffect } from "react";
 
 function Student() {
   const [students, setStudents] = useState([]);
+  const [editMode, setEditMode] = useState(false);
+  const [editedStudent, setEditedStudent] = useState({});
+  const [newStudent, setNewStudent] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    address: "",
+    phoneNumber: "",
+    studentNumber: "",
+    schoolId: "",
+  });
 
   useEffect(() => {
     fetchStudents();
-  }, []);
+  }, [editMode]);
 
   const fetchStudents = async () => {
     try {
@@ -17,9 +28,89 @@ function Student() {
     }
   };
 
+  const handleEdit = (student) => {
+    setEditMode(true);
+    setEditedStudent(student);
+  };
+
+  const handleCancel = () => {
+    setEditMode(false);
+    setEditedStudent({});
+  };
+
+  const handleSave = async () => {
+    try {
+      const response = await fetch(`/api/students/${editedStudent.id}`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(editedStudent),
+      });
+
+      if (response.ok) {
+        console.log("Updated:", editedStudent);
+        setEditMode(false);
+        setEditedStudent({});
+      } else {
+        console.error("Update failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleDelete = async (id) => {
+    try {
+      const response = await fetch(`/api/students/${id}`, {
+        method: "DELETE",
+      });
+
+      if (response.ok) {
+        console.log("Deleted:", id);
+        fetchStudents();
+      } else {
+        console.error("Delete failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
+  const handleCreate = async () => {
+    try {
+      const response = await fetch("/api/students", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newStudent),
+      });
+
+      if (response.ok) {
+        const createdStudent = await response.json();
+        setStudents([...students, createdStudent]);
+        console.log("Created:", createdStudent);
+        setNewStudent({
+          firstName: "",
+          lastName: "",
+          email: "",
+          address: "",
+          phoneNumber: "",
+          studentNumber: "",
+          schoolId: "",
+        });
+      } else {
+        console.error("Create failed");
+      }
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+
   return (
     <div className="flex flex-col">
-      <div className="-my-2 -px-2 ">
+      <div className="-my-2 -px-2">
         <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
           <div className="mt-2 shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
             <table className="min-w-full divide-y divide-gray-200">
@@ -49,6 +140,9 @@ function Student() {
                   <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
                     School Id
                   </th>
+                  <th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
+                    Actions
+                  </th>
                 </tr>
               </thead>
               <tbody className="bg-white divide-y divide-gray-200">
@@ -58,31 +152,238 @@ function Student() {
                       {student.id}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {student.firstName}
+                      {editMode && editedStudent.id === student.id ? (
+                        <input
+                          type="text"
+                          className="px-2 py-1 border rounded-lg"
+                          value={editedStudent.firstName || ""}
+                          onChange={(e) =>
+                            setEditedStudent({
+                              ...editedStudent,
+                              firstName: e.target.value,
+                            })
+                          }
+                        />
+                      ) : (
+                        student.firstName
+                      )}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {student.lastName}
+                      {editMode && editedStudent.id === student.id ? (
+                        <input
+                          type="text"
+                          className="px-2 py-1 border rounded-lg"
+                          value={editedStudent.lastName || ""}
+                          onChange={(e) =>
+                            setEditedStudent({
+                              ...editedStudent,
+                              lastName: e.target.value,
+                            })
+                          }
+                        />
+                      ) : (
+                        student.lastName
+                      )}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {student.email}
+                      {editMode && editedStudent.id === student.id ? (
+                        <input
+                          type="text"
+                          className="px-2 py-1 border rounded-lg"
+                          value={editedStudent.email || ""}
+                          onChange={(e) =>
+                            setEditedStudent({
+                              ...editedStudent,
+                              email: e.target.value,
+                            })
+                          }
+                        />
+                      ) : (
+                        student.email
+                      )}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {student.address}
+                      {editMode && editedStudent.id === student.id ? (
+                        <input
+                          type="text"
+                          className="px-2 py-1 border rounded-lg"
+                          value={editedStudent.address || ""}
+                          onChange={(e) =>
+                            setEditedStudent({
+                              ...editedStudent,
+                              address: e.target.value,
+                            })
+                          }
+                        />
+                      ) : (
+                        student.address
+                      )}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {student.phoneNumber}
+                      {editMode && editedStudent.id === student.id ? (
+                        <input
+                          type="text"
+                          className="px-2 py-1 border rounded-lg"
+                          value={editedStudent.phoneNumber || ""}
+                          onChange={(e) =>
+                            setEditedStudent({
+                              ...editedStudent,
+                              phoneNumber: e.target.value,
+                            })
+                          }
+                        />
+                      ) : (
+                        student.phoneNumber
+                      )}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {student.studentNumber}
+                      {editMode && editedStudent.id === student.id ? (
+                        <input
+                          type="text"
+                          className="px-2 py-1 border rounded-lg"
+                          value={editedStudent.studentNumber || ""}
+                          onChange={(e) =>
+                            setEditedStudent({
+                              ...editedStudent,
+                              studentNumber: e.target.value,
+                            })
+                          }
+                        />
+                      ) : (
+                        student.studentNumber
+                      )}
                     </td>
                     <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
-                      {student.schoolId}
+                      {editMode && editedStudent.id === student.id ? (
+                        <input
+                          type="text"
+                          className="px-2 py-1 border rounded-lg"
+                          value={editedStudent.schoolId || ""}
+                          onChange={(e) =>
+                            setEditedStudent({
+                              ...editedStudent,
+                              schoolId: e.target.value,
+                            })
+                          }
+                        />
+                      ) : (
+                        student.schoolId
+                      )}
+                    </td>
+                    <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+                      {editMode && editedStudent.id === student.id ? (
+                        <>
+                          <button
+                            className="mr-2 text-blue-500"
+                            onClick={handleSave}
+                          >
+                            Save
+                          </button>
+                          <button
+                            className="text-red-500"
+                            onClick={handleCancel}
+                          >
+                            Cancel
+                          </button>
+                        </>
+                      ) : (
+                        <>
+                          <button
+                            className="text-blue-500"
+                            onClick={() => handleEdit(student)}
+                          >
+                            Edit
+                          </button>
+                          <button
+                            className="text-red-500 ms-3"
+                            onClick={() => handleDelete(student.id)}
+                          >
+                            Delete
+                          </button>
+                        </>
+                      )}
                     </td>
                   </tr>
                 ))}
               </tbody>
             </table>
           </div>
+        </div>
+      </div>
+      <div className="my-4 ms-8">
+        <h2 className="text-gray-800 text-lg font-semibold text-gray mb-1">
+          Create New Student
+        </h2>
+        <div className="flex mt-2">
+          <input
+            type="text"
+            placeholder="First Name"
+            className="px-2 py-1 border rounded-lg mr-2"
+            value={newStudent.firstName}
+            onChange={(e) =>
+              setNewStudent({ ...newStudent, firstName: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Last Name"
+            className="px-2 py-1 border rounded-lg mr-2"
+            value={newStudent.lastName}
+            onChange={(e) =>
+              setNewStudent({ ...newStudent, lastName: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            className="px-2 py-1 border rounded-lg mr-2"
+            value={newStudent.email}
+            onChange={(e) =>
+              setNewStudent({ ...newStudent, email: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Address"
+            className="px-2 py-1 border rounded-lg mr-2"
+            value={newStudent.address}
+            onChange={(e) =>
+              setNewStudent({ ...newStudent, address: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Phone Number"
+            className="px-2 py-1 border rounded-lg mr-2"
+            value={newStudent.phoneNumber}
+            onChange={(e) =>
+              setNewStudent({ ...newStudent, phoneNumber: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="Student Number"
+            className="px-2 py-1 border rounded-lg mr-2"
+            value={newStudent.studentNumber}
+            onChange={(e) =>
+              setNewStudent({ ...newStudent, studentNumber: e.target.value })
+            }
+          />
+          <input
+            type="text"
+            placeholder="School Id"
+            className="px-2 py-1 border rounded-lg mr-2"
+            value={newStudent.schoolId}
+            onChange={(e) =>
+              setNewStudent({ ...newStudent, schoolId: e.target.value })
+            }
+          />
+          <button
+            className="px-4 py-2 bg-green-500 text-white rounded-lg"
+            onClick={handleCreate}
+          >
+            Create
+          </button>
         </div>
       </div>
     </div>
